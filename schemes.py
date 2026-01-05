fd8dbd8f86a6f9b17799e1a82945450211050d57
Blue112225 <58443888+winterniphredil@users.noreply.github.com>
2026-01-05 11:00:50 +0000

    added more schemes

--- schemes.py
+++ schemes.py
@@ -66,6 +66,17 @@ def ftcs_h(uOld,hOld,c,nx): # h first
     return uNew,hNew
 
 
+@scheme_wrapper("ftcs_sim")
+def ftcs_sim(uOld,hOld,c,nx): # h first
+    c = c/2.
+    D = D_mat(nx)
+    
+    hNew = hOld - c * (np.diagflat(uOld) @ D @ hOld + np.diagflat(hOld) @ D @ uOld)
+    uNew = uOld - c * (g * D @ hOld + np.diagflat(uOld) @ D @ uOld)
+    
+    return uNew,hNew
+
+
 @scheme_wrapper("ftbs_u")
 def ftbs_u(uOld,hOld,c,nx): # u first
     B = B_mat(nx)
@@ -82,6 +93,16 @@ def ftbs_h(uOld,hOld,c,nx): # h first
     
     hNew = hOld - c * (np.diagflat(uOld) @ B @ hOld + np.diagflat(hOld) @ B @ uOld)
     uNew = uOld - c * (g * B @ hNew + np.diagflat(uOld) @ B @ uOld)
+    
+    return uNew,hNew
+
+
+@scheme_wrapper("ftbs_sim")
+def ftbs_sim(uOld,hOld,c,nx): # h first
+    B = B_mat(nx)
+    
+    hNew = hOld - c * (np.diagflat(uOld) @ B @ hOld + np.diagflat(hOld) @ B @ uOld)
+    uNew = uOld - c * (g * B @ hOld + np.diagflat(uOld) @ B @ uOld)
     
     return uNew,hNew
 
@@ -166,6 +187,48 @@ def ftcs_semi_impl_h_3(uOld,hOld,c,nx): # u,h implicit, h first
         hNew[j] = (hOld[j] - c*(uOld[j]*(hOld[(j+1)%(nx+1)]-hOld[j-1]))) / (1 + c*(uOld[(j+1)%(nx+1)]-uOld[j]))
     for j in range(0,nx+1):
         uNew[j] = (uOld[j] - g*c*(hNew[(j+1)%(nx+1)]-hNew[j])) / (1 + c*(uOld[(j+1)%(nx+1)]-uOld[j-1]))
+        
+    return uNew,hNew
+
+
+@scheme_wrapper("ftcs_semi_impl_sim_1")
+def ftcs_semi_impl_sim_1(uOld,hOld,c,nx): # u implicit, h first
+    c = c/2.
+    uNew = uOld.copy()
+    hNew = hOld.copy()
+    
+    for j in range(0,nx+1):
+        hNew[j] = hOld[j] - c*(uOld[j]*(hOld[(j+1)%(nx+1)]-hOld[j-1]) + hOld[j]*(uOld[(j+1)%(nx+1)]-uOld[j-1]))
+    for j in range(0,nx+1):
+        uNew[j] = (uOld[j] - g*c*(hOld[(j+1)%(nx+1)]-hOld[j])) / (1 + c*(uOld[(j+1)%(nx+1)]-uOld[j-1]))
+    
+    return uNew,hNew
+
+
+@scheme_wrapper("ftcs_semi_impl_sim_2")
+def ftcs_semi_impl_sim_2(uOld,hOld,c,nx): # h implicit, h first
+    c = c/2.
+    uNew = uOld.copy()
+    hNew = hOld.copy()
+    
+    for j in range(0,nx+1):
+        hNew[j] = (hOld[j] - c*(uOld[j]*(hOld[(j+1)%(nx+1)]-hOld[j-1]))) / (1 + c*(uOld[(j+1)%(nx+1)]-uOld[j]))
+    for j in range(0,nx+1):
+        uNew[j] = uOld[j] - c*((uOld[j]*(uOld[(j+1)%(nx+1)]-uOld[j-1])) + g*(hOld[(j+1)%(nx+1)]-hOld[j-1]))
+    
+    return uNew,hNew
+
+
+@scheme_wrapper("ftcs_semi_impl_sim_3")
+def ftcs_semi_impl_sim_3(uOld,hOld,c,nx): # u,h implicit, h first
+    c = c/2.
+    uNew = uOld.copy()
+    hNew = hOld.copy()
+    
+    for j in range(0,nx+1):
+        hNew[j] = (hOld[j] - c*(uOld[j]*(hOld[(j+1)%(nx+1)]-hOld[j-1]))) / (1 + c*(uOld[(j+1)%(nx+1)]-uOld[j]))
+    for j in range(0,nx+1):
+        uNew[j] = (uOld[j] - g*c*(hOld[(j+1)%(nx+1)]-hOld[j])) / (1 + c*(uOld[(j+1)%(nx+1)]-uOld[j-1]))
         
     return uNew,hNew
 
